6ad623fc5811645a69f965b4d443b8111ac796a9
Nur Anisah <88489400+nuranisahzakaria@users.noreply.github.com>
2022-05-11 19:50:06 +0700

    Mengubah nama kota

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from geopy.geocoders import Nominatim
 
 Geolocater = Nominatim(user_agent="GeoLocation")
 
-Location = Geolocater.geocode("Jakarta")
+Location = Geolocater.geocode("Langkahan")
 
 print("Address: ", Location.address)
 print("Latitude: ", Location.latitude)
-print("Longitude", Location.longitude)
+print("Longitude: ", Location.longitude)
